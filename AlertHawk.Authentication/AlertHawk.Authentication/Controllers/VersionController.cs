5c3143d592cc794181660b706c0a1b6cfe9c8c98
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-01-24 21:46:01 +0100

    Initial Commit

---
+++ AlertHawk.Authentication/AlertHawk.Authentication/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace AlertHawk.Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        [SwaggerOperation(Summary = "Retrieve API version")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public string Get()
+        {
+            var version = Assembly.GetEntryAssembly()?.GetName().Version;
+            return version!.ToString();
+        }
+    }
+}
