87207aa092bb308ce4e29688ad7a10934a5be768
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-01-24 21:53:16 +0100

    Setup for AuthService

--- AlertHawk.Authentication/AlertHawk.Authentication/Program.cs
+++ AlertHawk.Authentication/AlertHawk.Authentication/Program.cs
@@ -1,3 +1,5 @@
+using EasyMemoryCache.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, false)
+    .AddEnvironmentVariables()
+    .Build();
+
+builder.Services.AddEasyCache(configuration.GetSection("CacheSettings").Get<CacheSettings>());
+builder.WebHost.UseSentry();
 
 var app = builder.Build();
 
@@ -19,6 +30,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSentryTracing();
 
 app.MapControllers();
 
