806e947794bb8c551fbe34ddb27459769bf3c3ad
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-01-25 09:22:16 +0100

    Packages update and small change

--- AlertHawk.Authentication/AlertHawk.Authentication.Tests/UnitTest1.cs
+++ AlertHawk.Authentication/AlertHawk.Authentication.Tests/UnitTest1.cs
@@ -3,7 +3,8 @@ namespace AlertHawk.Authentication.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void should_return_true()
     {
+        Assert.True(true);
     }
 }
