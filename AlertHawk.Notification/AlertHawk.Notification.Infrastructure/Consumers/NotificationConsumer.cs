b2a5b56a2473e2d18ac02dd92c5dd3f82d2e9268
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-01-26 21:33:04 +0100

    Notify test to Slack

--- AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Consumers/NotificationConsumer.cs
+++ AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Consumers/NotificationConsumer.cs
@@ -1,11 +1,22 @@
+using AlertHawk.Notification.Domain.Interfaces.Notifiers;
 using MassTransit;
 
 namespace SharedModels;
 
 public class NotificationConsumer : IConsumer<NotificationAlert>
 {
+    private readonly ISlackNotifier _slackNotifier;
+
+    public NotificationConsumer(ISlackNotifier slackNotifier)
+    {
+        _slackNotifier = slackNotifier;
+    }
+
     public async Task Consume(ConsumeContext<NotificationAlert> context)
     {
+        var webHookUrl = Environment.GetEnvironmentVariable("slack-webhookurl");
+
+        await _slackNotifier.SendNotification("alerthawk-test", context.Message.Message, webHookUrl);
         // Handle the received message
         Console.WriteLine(
             $"NotificationId: {context.Message.NotificationId} Notification Message: {context.Message.Message}");
