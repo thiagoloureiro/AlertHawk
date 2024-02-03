a2eb23d6a3e135bb02c9164247b3bee63ab7e1c9
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-02-03 19:22:22 +0100

    test

--- AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Notifiers/SlackNotifier.cs
+++ AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Notifiers/SlackNotifier.cs
@@ -15,10 +15,15 @@ namespace AlertHawk.Notification.Infrastructure.Notifiers
                 using HttpClient httpClient = new HttpClient();
                 string payload = $"{{\"channel\": \"{channel}\", \"text\": \"{message}\"}}";
 
+                Console.WriteLine(payload);
                 using StringContent content = new StringContent(payload, Encoding.UTF8, "application/json");
 
                 using HttpResponseMessage response = await httpClient.PostAsync(webHookUrl, content);
 
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.Content.ReadAsStringAsync());
+                }
                 response.EnsureSuccessStatusCode();
 
                 Console.WriteLine("Notification sent successfully!");
