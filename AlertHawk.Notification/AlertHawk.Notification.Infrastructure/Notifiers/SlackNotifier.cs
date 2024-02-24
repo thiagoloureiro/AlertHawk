9e499f180e7cf51c3e5d563d2814fafe17fb35f4
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-02-25 00:09:47 +0100

    cleanup

--- AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Notifiers/SlackNotifier.cs
+++ AlertHawk.Notification/AlertHawk.Notification.Infrastructure/Notifiers/SlackNotifier.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using AlertHawk.Notification.Domain.Interfaces.Notifiers;
-using Sentry;
 
 namespace AlertHawk.Notification.Infrastructure.Notifiers
 {
@@ -8,26 +7,15 @@ namespace AlertHawk.Notification.Infrastructure.Notifiers
     {
         public async Task SendNotification(string channel, string message, string webHookUrl)
         {
-            try
-            {
-                Console.WriteLine(
-                    $"Sending notification channel: {channel}, message: {message} webhookUrl: {webHookUrl}");
-                using HttpClient httpClient = new HttpClient();
-                string payload = $"{{\"channel\": \"{channel}\", \"text\": \"{message}\"}}";
+            using HttpClient httpClient = new HttpClient();
+            string payload = $"{{\"channel\": \"{channel}\", \"text\": \"{message}\"}}";
 
-                Console.WriteLine(payload);
-                using StringContent content = new StringContent(payload, Encoding.UTF8, "application/json");
+            Console.WriteLine(payload);
+            using StringContent content = new StringContent(payload, Encoding.UTF8, "application/json");
 
-                using HttpResponseMessage response = await httpClient.PostAsync(webHookUrl, content);
-                
-                response.EnsureSuccessStatusCode();
+            using HttpResponseMessage response = await httpClient.PostAsync(webHookUrl, content);
 
-                Console.WriteLine("Notification sent successfully!");
-            }
-            catch (Exception e)
-            {
-                SentrySdk.CaptureException(e);
-            }
+            response.EnsureSuccessStatusCode();
         }
     }
 }
