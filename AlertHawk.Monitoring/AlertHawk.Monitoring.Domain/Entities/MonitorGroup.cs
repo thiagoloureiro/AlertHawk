068b2871efa1f89a80f9804f4321ca15246ebc41
Thiago Loureiro <thiagoguaru@me.com>
2024-03-26 16:19:28 +0100

    quick fix

--- AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Entities/MonitorGroup.cs
+++ AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Entities/MonitorGroup.cs
@@ -5,10 +5,10 @@ public class MonitorGroup
     public int Id { get; set; }
     public required string Name { get; set; }
     public IEnumerable<Monitor>? Monitors { get; set; }
-    public double AvgUptime1Hr { get; set; }
-    public double AvgUptime24Hrs { get; set; }
-    public double AvgUptime7Days { get; set; }
-    public double AvgUptime30Days { get; set; }
-    public double AvgUptime3Months { get; set; }
-    public double AvgUptime6Months { get; set; }
+    public double? AvgUptime1Hr { get; set; }
+    public double? AvgUptime24Hrs { get; set; }
+    public double? AvgUptime7Days { get; set; }
+    public double? AvgUptime30Days { get; set; }
+    public double? AvgUptime3Months { get; set; }
+    public double? AvgUptime6Months { get; set; }
 }
