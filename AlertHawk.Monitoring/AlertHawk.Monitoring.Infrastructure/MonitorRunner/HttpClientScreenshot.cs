139d53c2f2029c32a2bdce2444123505a35f630b
Thiago Loureiro <thiagoguaru@me.com>
2024-04-05 11:57:29 +0200

    Try/Catch on screenshot mechanism

--- AlertHawk.Monitoring/AlertHawk.Monitoring.Infrastructure/MonitorRunner/HttpClientScreenshot.cs
+++ AlertHawk.Monitoring/AlertHawk.Monitoring.Infrastructure/MonitorRunner/HttpClientScreenshot.cs
@@ -9,50 +9,58 @@ public class HttpClientScreenshot : IHttpClientScreenshot
 {
     public async Task<string> TakeScreenshotAsync(string url, int monitorId, string monitorName)
     {
-        var screenshotEnabled = VariableUtils.GetBoolEnvVariable("enable_screenshot");
-        string screenshotUrl = string.Empty;
-        if (screenshotEnabled)
+        try
         {
-            // Set the path to the directory where you want to save the screenshot
-            string screenshotDirectory = $@"/screenshots/{monitorId}_{monitorName}";
-            // Ensure the directory exists, create it if necessary
-            Directory.CreateDirectory(screenshotDirectory);
-
-            // Set Chrome options
-            ChromeOptions options = new ChromeOptions();
-            options.AddArguments("--no-sandbox");
-            options.AddArguments("--disable-dev-shm-usage");
-            options.AddArgument("--headless");
-            options.AddArgument("--window-size=1280,780");
-
-            // Initialize ChromeDriver
-            using var driver = new ChromeDriver(options);
-
-            driver.Navigate().GoToUrl(url);
-
-            // Wait for the page to load (adjust the wait time as needed)
-            Thread.Sleep(VariableUtils.GetIntEnvVariable("screenshot_wait_time_ms") ?? 3000);
-
-            // Take screenshot
-            Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-
-            // Generate file name
-            string fileName = $"screenshot_monitorId_{monitorId}_{DateTime.UtcNow:yyyy-MM-dd_HH_mm_ss}.png";
-
-            // Save the screenshot
-            string filePath = Path.Combine(screenshotDirectory, fileName);
-            screenshot.SaveAsFile(filePath);
-            var screenshotAsByteArray = screenshot.AsByteArray;
-
-            if (VariableUtils.GetBoolEnvVariable("enable_screenshot_storage_account"))
+            var screenshotEnabled = VariableUtils.GetBoolEnvVariable("enable_screenshot");
+            string screenshotUrl = string.Empty;
+            if (screenshotEnabled)
             {
-                screenshotUrl =
-                    await BlobUtils.UploadByteArrayToBlob($"{monitorId}-{monitorName}.jpg", screenshotAsByteArray);
+                // Set the path to the directory where you want to save the screenshot
+                string screenshotDirectory = $@"/screenshots/{monitorId}_{monitorName}";
+                // Ensure the directory exists, create it if necessary
+                Directory.CreateDirectory(screenshotDirectory);
+
+                // Set Chrome options
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--no-sandbox");
+                options.AddArguments("--disable-dev-shm-usage");
+                options.AddArgument("--headless");
+                options.AddArgument("--window-size=1280,780");
+
+                // Initialize ChromeDriver
+                using var driver = new ChromeDriver(options);
+
+                driver.Navigate().GoToUrl(url);
+
+                // Wait for the page to load (adjust the wait time as needed)
+                Thread.Sleep(VariableUtils.GetIntEnvVariable("screenshot_wait_time_ms") ?? 3000);
+
+                // Take screenshot
+                Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+
+                // Generate file name
+                string fileName = $"screenshot_monitorId_{monitorId}_{DateTime.UtcNow:yyyy-MM-dd_HH_mm_ss}.png";
+
+                // Save the screenshot
+                string filePath = Path.Combine(screenshotDirectory, fileName);
+                screenshot.SaveAsFile(filePath);
+                var screenshotAsByteArray = screenshot.AsByteArray;
+
+                if (VariableUtils.GetBoolEnvVariable("enable_screenshot_storage_account"))
+                {
+                    screenshotUrl =
+                        await BlobUtils.UploadByteArrayToBlob($"{monitorId}-{monitorName}.jpg", screenshotAsByteArray);
+                }
+
+                Console.WriteLine($"Screenshot saved: {filePath}");
             }
 
-            Console.WriteLine($"Screenshot saved: {filePath}");
+            return screenshotUrl;
         }
-
-        return screenshotUrl;
+        catch (Exception e)
+        {
+            SentrySdk.CaptureException(e);
+            return null;
+        }
     }
 }
