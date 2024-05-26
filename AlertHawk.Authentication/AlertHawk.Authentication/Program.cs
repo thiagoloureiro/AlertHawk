3c1a129f4c1edff763865453cdb97b341623c558
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-05-26 06:46:00 +0200

    [assembly: ExcludeFromCodeCoverage]

--- AlertHawk.Authentication/AlertHawk.Authentication/Program.cs
+++ AlertHawk.Authentication/AlertHawk.Authentication/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.OpenApi.Models;
 using System.Reflection;
 using Microsoft.Identity.Web;
 
+[assembly: ExcludeFromCodeCoverage]
 var builder = WebApplication.CreateBuilder(args);
 
 var configuration = new ConfigurationBuilder()
