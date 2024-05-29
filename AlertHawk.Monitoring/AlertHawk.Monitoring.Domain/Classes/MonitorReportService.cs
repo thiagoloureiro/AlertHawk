995508020a59db5df4447f25cb172802a92203b0
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-05-29 16:03:54 +0200

    calculating the uptime % for the results.

--- AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Classes/MonitorReportService.cs
+++ AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Classes/MonitorReportService.cs
@@ -41,6 +41,16 @@ public class MonitorReportService : IMonitorReportService
     public async Task<IEnumerable<MonitorReportUptime>> GetMonitorReportUptime(int groupId, DateTime startDate,
         DateTime endDate)
     {
-        return await _monitorReportRepository.GetMonitorReportUptime(groupId, startDate, endDate);
+        var result = await _monitorReportRepository.GetMonitorReportUptime(groupId, startDate, endDate);
+
+        var monitorReportUptimes = result.ToList();
+        foreach (var report in monitorReportUptimes)
+        {
+            double uptimePercentage = (double)report.TotalOnlineMinutes /
+                (report.TotalOnlineMinutes + report.TotalOfflineMinutes) * 100;
+            report.UptimePercentage = uptimePercentage;
+        }
+
+        return monitorReportUptimes;
     }
 }
