7b20deea793a1f1ca6db30cfd0c43bad934d77a0
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-06-12 05:03:22 +0200

    [JsonIgnore] on some unused properties

--- AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Entities/MonitorHistory.cs
+++ AlertHawk.Monitoring/AlertHawk.Monitoring.Domain/Entities/MonitorHistory.cs
@@ -1,13 +1,20 @@
+using System.Text.Json.Serialization;
+
 namespace AlertHawk.Monitoring.Domain.Entities;
 
 public class MonitorHistory
 {
+    [JsonIgnore]
     public long Id { get; set; }
+    [JsonIgnore]
     public int MonitorId { get; set; }
+    [JsonIgnore]
     public bool Status { get; set; }
     public DateTime TimeStamp { get; set; }
+    [JsonIgnore]
     public int StatusCode { get; set; }
     public int ResponseTime { get; set; }
+    [JsonIgnore]
     public string? HttpVersion { get; set; }
     public string? ResponseMessage { get; set; }
     public string? ScreenShotUrl { get; set; }
