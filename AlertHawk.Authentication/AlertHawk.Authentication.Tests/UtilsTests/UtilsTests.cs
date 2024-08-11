a3059ff90147b025a94f78d6731eb42d197f4e74
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-08-11 22:32:32 +0200

    + Tests

--- AlertHawk.Authentication/AlertHawk.Authentication.Tests/UtilsTests/UtilsTests.cs
+++ AlertHawk.Authentication/AlertHawk.Authentication.Tests/UtilsTests/UtilsTests.cs
@@ -1,4 +1,6 @@
+using AlertHawk.Authentication.Infrastructure.Helpers;
 using AlertHawk.Authentication.Infrastructure.Utils;
+using Microsoft.AspNetCore.Identity;
 
 namespace AlertHawk.Authentication.Tests.UtilsTests;
 
@@ -21,7 +23,7 @@ public class UtilsTests
     public void TokenUtilsTests_GetJwtTokenFromRequest_InvalidRequest_ReturnsNull()
     {
         // Arrange
-        string? request = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        string request = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
 
         // Act
         var result = TokenUtils.GetJwtToken(request);
@@ -34,7 +36,8 @@ public class UtilsTests
     public void TokenUtilsTests_GetJwtTokenFromRequest_ReturnsToken()
     {
         // Arrange
-        string token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c\n";
+        string token =
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c\n";
         string[] tokenParts = token.Split(' ');
 
         // Act
@@ -42,5 +45,34 @@ public class UtilsTests
 
         // Assert
         Assert.Equal(tokenParts[1], result);
+    }
+
+    [Fact]
+    public void PasswordHasherTests_HashPassword_ReturnsHashedPassword()
+    {
+        // Arrange
+        string password = "password";
+
+        // Act
+        var salt = PasswordHasher.GenerateSalt();
+        var result = PasswordHasher.HashPassword(password, salt);
+        var verifyPassword = PasswordHasher.VerifyPassword(password, result, salt);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(verifyPassword);
+    }
+
+    [Fact]
+    public void PasswordHasherTests_HashPassword_ReturnsRandomPassword()
+    {
+        // Arrange
+        int lenght = 10;
+
+        // Act
+        var result = PasswordHasher.GenerateRandomPassword(lenght);
+
+        // Assert
+        Assert.NotNull(result);
     }
 }
