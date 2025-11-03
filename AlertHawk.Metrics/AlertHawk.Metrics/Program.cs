0a01922674c303f8af2aa296423cff115e6df079
Thiago Loureiro <thiagoguaru@me.com>
2025-11-03 18:17:15 -0500

    Refactor pod listing to watch specific namespaces

--- AlertHawk.Metrics/AlertHawk.Metrics/Program.cs
+++ AlertHawk.Metrics/AlertHawk.Metrics/Program.cs
@@ -3,16 +3,20 @@
 var config = KubernetesClientConfiguration.InClusterConfig();
 var client = new Kubernetes(config);
 
-var pods = await client.CoreV1.ListPodForAllNamespacesAsync();
-
-foreach (var pod in pods.Items)
+var namespacesToWatch = new[] { "alerthawk", "traefik", "ilstudio" };
+foreach (var ns in namespacesToWatch)
 {
-    Console.WriteLine($"{pod.Metadata.NamespaceProperty}/{pod.Metadata.Name} - {pod.Status.Phase}");
+    var pods = await client.CoreV1.ListNamespacedPodAsync(ns);
+
+    foreach (var pod in pods.Items)
+    {
+        Console.WriteLine($"{pod.Metadata.NamespaceProperty}/{pod.Metadata.Name} - {pod.Status.Phase}");
+    }
+
+    var response = await client.CustomObjects.ListClusterCustomObjectAsync(
+        group: "metrics.k8s.io",
+        version: "v1beta1",
+        plural: "pods");
+
+    Console.WriteLine(response);
 }
-
-var response = await client.CustomObjects.ListClusterCustomObjectAsync(
-    group: "metrics.k8s.io",
-    version: "v1beta1",
-    plural: "pods");
-
-Console.WriteLine(response);
