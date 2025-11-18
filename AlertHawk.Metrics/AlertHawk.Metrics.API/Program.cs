ad356b59ccaaafe23c2479a4b2b4a7cfb303e1d9
Thiago Loureiro <thiagoguaru@me.com>
2025-11-18 00:02:56 -0500

    Add Swagger documentation and version endpoint to Metrics API

--- AlertHawk.Metrics/AlertHawk.Metrics.API/Program.cs
+++ AlertHawk.Metrics/AlertHawk.Metrics.API/Program.cs
@@ -1,10 +1,34 @@
+using System.Reflection;
 using AlertHawk.Metrics;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "AlertHawk Metrics API",
+            Version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString()
+        });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description =
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+});
 
 // Register ClickHouse service
 var clickHouseConnectionString = Environment.GetEnvironmentVariable("CLICKHOUSE_CONNECTION_STRING")
@@ -22,7 +46,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    var basePath = Environment.GetEnvironmentVariable("basePath") ?? "";
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "swagger/{documentName}/swagger.json";
+        c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+        {
+            swaggerDoc.Servers = new List<OpenApiServer>
+                { new OpenApiServer { Url = $"https://{httpReq.Host.Value}{basePath}" } };
+        });
+    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
