dbd740d49a28274b98bf943dded25f889a65d33b
Thiago Loureiro <thiagoguaru@me.com>
2025-11-20 16:43:16 -0500

    Update Sentry DSN configuration to use environment variable

--- AlertHawk.Metrics/AlertHawk.Metrics/Program.cs
+++ AlertHawk.Metrics/AlertHawk.Metrics/Program.cs
@@ -19,13 +19,14 @@ try
         // A Sentry Data Source Name (DSN) is required.
         // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
         // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
-        options.Dsn = "https://7539147312d4c51ccf970c6ddd0f15ca@o418696.ingest.us.sentry.io/4510386963283968";
+        options.Dsn = Environment.GetEnvironmentVariable("SENTRY_DSN") ?? "https://7539147312d4c51ccf970c6ddd0f15ca@o418696.ingest.us.sentry.io/4510386963283968";
 
         // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
         // This might be helpful, or might interfere with the normal operation of your application.
         // We enable it here for demonstration purposes when first trying Sentry.
         // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
         options.Debug = false;
+        options.Environment = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "Production";
 
         // This option is recommended. It enables Sentry's "Release Health" feature.
         options.AutoSessionTracking = true;
