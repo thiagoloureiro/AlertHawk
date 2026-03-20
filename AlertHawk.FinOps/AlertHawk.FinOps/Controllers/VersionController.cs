31f6a3cf89e963e99cdcba53fd2f633531427465
Thiago Loureiro de Azevedo <thiago.loureiro+abbemu@us.abb.com>
2026-03-20 11:12:09 -0400

    version controller

---
+++ AlertHawk.FinOps/AlertHawk.FinOps/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace AlertHawk.FinOps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ExcludeFromCodeCoverage]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public string Get()
+        {
+            var version = Assembly.GetEntryAssembly()?.GetName().Version;
+            return version!.ToString();
+        }
+    }
+}
